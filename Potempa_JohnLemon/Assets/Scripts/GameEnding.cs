1029ce36b869218023fadad32c05fcd4ed3e4d4a
mappotem <mappotem@iu.edu>
2019-10-06 23:55:25 -0400

    edited game ending and observer scripts

--- Potempa_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Potempa_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,14 +9,20 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     float m_Timer;
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
 
     private void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
@@ -27,13 +34,26 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-    void EndLevel()
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
         if(m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
