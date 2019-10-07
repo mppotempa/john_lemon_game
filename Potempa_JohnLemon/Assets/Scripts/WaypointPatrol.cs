611ab067e182e1ac708e052b5ff5fd724943dc13
mappotem <mappotem@iu.edu>
2019-10-07 00:23:55 -0400

    created waypoint script

---
+++ Potempa_JohnLemon/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int m_CurrencyWaypointIndex;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+   
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrencyWaypointIndex = (m_CurrencyWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrencyWaypointIndex].position);
+        }
+    }
+}
