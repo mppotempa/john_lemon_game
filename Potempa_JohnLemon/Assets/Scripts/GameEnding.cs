a82437a9b6d0172764e20237ccb798692b3b0a5f
mappotem <mappotem@iu.edu>
2019-10-10 15:45:01 -0400

    added time calculating script

--- Potempa_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Potempa_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
+
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
     public AudioSource exitAudio;
     public AudioSource caughtAudio;
+    public Text timeText;
+
     float m_Timer;
+    float m_TimePassed;
     bool m_HasAudioPlayed;
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
@@ -26,6 +31,37 @@ public class GameEnding : MonoBehaviour
         else if (m_IsPlayerCaught)
         {
             EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+        else
+        {
+            //display time
+            m_TimePassed += Time.deltaTime;
+
+            //min
+            int min = Mathf.FloorToInt(m_TimePassed % 60f);
+            string minString = min.ToString();
+            if (min < 10)
+            {
+                minString = "0" + minString;
+
+            }
+
+            //hour
+            int hour = (Mathf.FloorToInt(m_TimePassed / 60f)) % 12;
+            if(hour == 0)
+            {
+                hour += 12;
+            }
+
+            //AM or PM
+            string ampm = "AM";
+            if(((m_TimePassed/60f) %  24f) > 12f)
+            {
+                ampm = "PM";
+            }
+
+            //output time
+            timeText.text = hour.ToString() + ":" + minString + ampm;
         }
     }
 
